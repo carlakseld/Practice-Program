2e36ba5cace133a758e233d63b1d9d571fe5d113
carlakseld <carlakseld@gmail.com>
2026-02-11 07:16:11 +0800

    Add files via upload

---
+++ LoopDemo.java
@@ -0,0 +1,25 @@
+public class LoopDemo{
+	public static void main(String aksel[]){
+
+	System.out.println("For loop:");
+	for(int i = 1; i <= 5; i++){
+		System.out.println(i);
+	}
+	System.out.println("While loop:");
+	int j = 1;
+	while(j <=5){
+		System.out.println(j);
+		j++;
+	}
+	System.out.println("do-while loop:");
+	int n = 1;
+	do{
+		System.out.println(n);
+		n++;
+	}while(n <= 5);
+
+
+
+
+	}
+}
